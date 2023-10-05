aab081b214618ba8b2c026a7f759a7fe14e012d1
SeHyun11103 <dotori506@gmail.com>
2023-10-05 16:23:58 +0900

    4

--- main.c
+++ main.c
@@ -3,17 +3,19 @@
 
 int main(int argc, char *argv[])
 {
-    char c;
-    int num=0;
-    printf("input a string : ");
-    while (( c=getchar() ) !='\n')
+    int sum = 0;
+    int num;
+    int i;
+    
+    printf("input a number : ");
+    scanf("%d",&num);
+    
+    for (i=0; i<=num ; i++)
     {
-                  if (c>='0'&&c<='9')
-                  {
-                                     num=num+1;
-                                     }
-                  }
-    printf("no. of digits is %i \n",num);
+        sum=sum+i;
+        }
+        
+        printf("result is : %d",sum);
   system("PAUSE");	
   return 0;
 }
